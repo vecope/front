2705ecc152a245ad340dbc2a6391185c742f1b8d
Camilo Alfonso <ca.alfonso967@uniandes.edu.co>
2017-09-18 16:49:54 -0500

    Error

---
+++ src/login.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'React';
+
+export default class loginModule extends Component{
+
+
+	constructor (props){	
+		super(props);
+		this.state = null;
+	}
+
+	render(){
+		return(<h1>HOLA</h1>);
+	}
+}
